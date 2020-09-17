7b38df3145c96831cac47ae995c1e86e8c53d8ba
gawgaw <gawgaw.developer@gmail.com>
2020-09-17 09:08:54 +0900

    CSSが2周読み込まれていたので修正

--- login/staff_login.php
+++ login/staff_login.php
@@ -21,8 +21,6 @@ $_SESSION = [];
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../css/reset.css">
   <link rel="stylesheet" href="../css/style.css">
-  <link rel="stylesheet" href="../css/reset.css">
-  <link rel="stylesheet" href="../css/style.css">
   <title>スタッフログイン</title>
 </head>
 <body>
